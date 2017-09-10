4aca2e6ac02b19d6a419f697dcbdacc6cf0c4104
Gio85 <giovanni.galiero85@gmail.com>
2017-09-10 15:33:45 +0100

    fixed directors view

---
+++ controllers/sessions.js
@@ -0,0 +1,29 @@
+const User = require('../models/user');
+
+function sessionsNew(req, res) {
+  res.render('sessions/new');
+}
+
+function sessionsCreate(req, res) {
+  User
+    .findOne({email: req.body.email })
+    .then(user => {
+      if(!user || !user.validatePassword(req.body.password)) {
+        req.flash('danger', 'Invalid credentials' );
+        return res.redirect('/login');
+      }
+      // user is authenticated
+      req.session.userId = user.id; // ora e' strored and crypted onto the browser
+      res.redirect('/');
+    });
+}
+function sessionsDelete(req, res){
+  req.session.regenerate(() => res.redirect('/'));
+}
+
+
+module.exports = {
+  new: sessionsNew,
+  create: sessionsCreate,
+  delete: sessionsDelete
+};
