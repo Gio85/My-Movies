22e3423431541693d6a026bdad462ed8a16990c6
Gio85 <giovanni.galiero85@gmail.com>
2017-09-11 08:43:12 +0100

    added js folder and file

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
-console.log('Hello world');
+const $burger = $('.navbar-burger');
+const $menu = $('.navbar-menu');
+const $directorForm = $('#director-form');
+const $form = $('form');
+
+$burger.on('click', () => {
+  $burger.toggleClass('is-active');
+  $menu.toggleClass('is-active');
+});
+
+if($directorForm.length > 0) $directorForm.find('select').on('change', () => $directorForm.trigger('submit'));
+
+if($form.length>0) $form.validate();
